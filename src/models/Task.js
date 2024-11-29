96ddf278288864045bc924a18dc85b27dc9e792e
encattani <encattani@hotmail.com>
2024-11-28 22:00:43 -0300

    fix: task status type

--- src/models/Task.js
+++ src/models/Task.js
@@ -4,7 +4,7 @@ const taskSchema = new mongoose.Schema({
   _id: { type: mongoose.Schema.Types.ObjectId, auto: true },
   title: { type: String, required: true },
   description: { type: String },
-  status: { type: String, default: "pendente" },
+  status: { type: Boolean, default: false },
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now },
   user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
