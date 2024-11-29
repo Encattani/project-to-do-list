fdfa744a4da9ea3e1b5ae6f2a0f36457eb041dea
encattani <encattani@hotmail.com>
2024-11-29 08:03:54 -0300

    refact: auth controller refactored and data validation implemented

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,30 +1,49 @@
-const User = require("../models/User");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+const { userSchema } = require("../utils/validationSchemas");
 
 exports.register = async (req, res) => {
   try {
+    const { error } = userSchema.validate(req.body);
+    if (error)
+      return res.status(400).json({ message: error.details[0].message });
+
     const { name, email, password } = req.body;
-    const user = new User({ name, email, password });
+
+    const userExists = await User.findOne({ email });
+    if (userExists)
+      return res.status(400).json({ message: "Usuário já existe!" });
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = new User({ name, email, password: hashedPassword });
     await user.save();
-    res.status(201).send({ message: `Usuário criado com sucesso!` });
+
+    res.status(201).json({ message: "Usuário registrado com sucesso!" });
   } catch (error) {
-    res.status(400).send({ error: error.message });
+    res.status(500).json({ message: error.message });
   }
 };
 
 exports.login = async (req, res) => {
   try {
-    const { email, senha } = req.body;
-    const user = await User.findOne({ email });
+    const { error } = userSchema.validate(req.body);
+    if (error)
+      return res.status(400).json({ message: error.details[0].message });
 
-    if (!user || !(await bcrypt.compare(senha, user.senha))) {
-      return res.status(401).send({ message: `Credenciais inválidas` });
-    }
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) return res.status(404).json({ message: "Usuário não enontrado." });
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid)
+      return res.status(400).json({ message: "Credenciais inválidas." });
+
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
       expiresIn: "1h",
     });
-    res.send({ token });
+    res.status(200).json({ token });
   } catch (error) {
-    res.status(500).send({ error: error.message });
+    res.status(500).json({ message: error.message });
   }
 };
