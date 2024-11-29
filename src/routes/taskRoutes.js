bb1af71a506e8a476057ee9b43498da71a8e83c4
encattani <encattani@hotmail.com>
2024-11-29 02:40:54 -0300

    fix: task routes renamed

--- src/routes/taskRoutes.js
+++ src/routes/taskRoutes.js
@@ -4,8 +4,8 @@ const authMiddleware = require('../middleware/authMiddleware');
 const router = express.Router();
 
 router.post('/', authMiddleware, createTask);
-router.put('/:id', authMiddleware, updateTask);
-router.delete('/:id', authMiddleware, deleteTask);
-router.get('/', authMiddleware, getTasks);
+router.put('/update/:id', authMiddleware, updateTask);
+router.delete('/delete/:id', authMiddleware, deleteTask);
+router.get('/getTasks', authMiddleware, getTasks);
 
 module.exports = router;
